9e6657efbf823fc9c62d2352549873e3017f4373
Lambros Chatzinikolaou <39900010+laxatzi@users.noreply.github.com>
2018-08-06 12:19:01 +0300

    Update omnifoodProject.js

--- omnifoodProject.js
+++ omnifoodProject.js
@@ -1,73 +1,56 @@
-$(document).ready(function()  {
+$(document).ready(function() {
 	
-	$('.section-features--js').waypoint(function(direction) {
-		
-		if (direction == "down")  {
-			$('nav').addClass('sticky-navigation');
+    $('.section-features--js').waypoint(function(direction) {
+        if (direction == "down") {
+	   $('nav').addClass('sticky-navigation');
 			
-		}
-		else {
-			$('nav').removeClass('sticky-navigation');
-			
-		}
-		},
-		{
+	} else {
+	   $('nav').removeClass('sticky-navigation');
+	}
+    }, {
        offset: '25%' 
-    
+    });
 	
-	});
-	
-	
-	
-  /*    THIS IS THE PLUGIN DOC GUIDE
-
+ /*    THIS IS THE PLUGIN DOC GUIDE
 var waypoints = $('#handler-first').waypoint(function(direction) {
-  notify(this.element.id + ' hit 25% from top of window') 
+   notify(this.element.id + ' hit 25% from top of window') 
 }, {
   offset: '25%'
 })	 */
 	
-	/* Scrolling */
-			 $('.scroll-to-plans--js').click(function(){
-				
-			 $('html, body').animate( {scrollTop:$('.section-plans--js').offset().top}, 1000);
-			});   /* End of scrolling to plans script */
+/* Scrolling */
+   $('.scroll-to-plans--js').click(function() {
+      $('html, body').animate( { scrollTop:$('.section-plans--js').offset().top }, 1000);
+});   /* End of scrolling to plans script */
 			         
-			$('.scroll-to-features--js').click(function(){
+   $('.scroll-to-features--js').click(function() {
+      $('html, body').animate( {scrollTop:$('.section-features--js').offset().top}, 1000);
+});    /* End of scrolling to features script */
+			
+/* Scrolling through menu */		
 		
-			 $('html, body').animate( {scrollTop:$('.section-features--js').offset().top}, 1000);
-			});    /* End of scrolling to features script */
+   $('.delivery--js').click(function() {
+      $('html, body').animate( {scrollTop:$('.section-gallery--js').offset().top}, 1000);
+});   /* End of scrolling to gallery script */
 			
-	/* Scrolling through menu */		
-		
-			$('.delivery--js').click(function(){
-				
-			 $('html, body').animate( {scrollTop:$('.section-gallery--js').offset().top}, 1000);
-			});   /* End of scrolling to gallery script */
+   $('.howTo--js').click(function() {
+      $('html, body').animate( {scrollTop:$('.section-steps--js').offset().top}, 1000);
+});   /* End of scrolling to how to/steps script */
 			
-			$('.howTo--js').click(function(){
-				
-			 $('html, body').animate( {scrollTop:$('.section-steps--js').offset().top}, 1000);
-			});   /* End of scrolling to how to/steps script */
-			
-			$('.cities--js').click(function(){
-				
-			 $('html, body').animate( {scrollTop:$('.section-cities--js').offset().top}, 1000);
-			});   /* End of scrolling to cities script */
-			
-			$('.contact--js').click(function(){
-				
-			 $('html, body').animate( {scrollTop:$('.section-contactform--js').offset().top}, 1000);
-			});   /* End of scrolling to contactForm script */
-		
-	/* Toggle */
+   $('.cities--js').click(function() {
+      $('html, body').animate( {scrollTop:$('.section-cities--js').offset().top}, 1000);
+});   /* End of scrolling to cities script */
 	
-	       function toggleFunc() {
-								   var menu = document.querySelectorAll(".main-navigation");
-								   menu.forEach( function (arg){
-																return arg.classList.toggle("navbar-menu--show")
-																});
-								}
-   
-					document.querySelector(".hamburger").addEventListener("click", toggleFunc);
+   $('.contact--js').click(function() {
+      $('html, body').animate( {scrollTop:$('.section-contactform--js').offset().top}, 1000);
+});   /* End of scrolling to contactForm script */
+		
+/* Toggle */
+   function toggleFunc() {
+      var menu = document.querySelectorAll(".main-navigation");
+      menu.forEach( function (arg) {
+         return arg.classList.toggle("navbar-menu--show")
+         });
+   }								
+   document.querySelector(".hamburger").addEventListener("click", toggleFunc);
 });

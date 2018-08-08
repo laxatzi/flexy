9a8d38d47583bde3d129223609c2522976a064cb
Lambros Chatzinikolaou <39900010+laxatzi@users.noreply.github.com>
2018-08-08 11:06:45 +0300

    Update omnifoodProject.js

--- omnifoodProject.js
+++ omnifoodProject.js
@@ -18,34 +18,34 @@ var waypoints = $('#handler-first').waypoint(function(direction) {
   offset: '25%'
 })	 */
 	
-/* Scrolling */
+// Scrolling 
    $('.scroll-to-plans--js').click(function() {
       $('html, body').animate( { scrollTop:$('.section-plans--js').offset().top }, 1000);
-});   /* End of scrolling to plans script */
+});   // End of scrolling to plans script 
 			         
    $('.scroll-to-features--js').click(function() {
       $('html, body').animate( {scrollTop:$('.section-features--js').offset().top}, 1000);
-});    /* End of scrolling to features script */
+});    // End of scrolling to features script 
 			
-/* Scrolling through menu */		
+// Scrolling through menu
 		
    $('.delivery--js').click(function() {
       $('html, body').animate( {scrollTop:$('.section-gallery--js').offset().top}, 1000);
-});   /* End of scrolling to gallery script */
+});   // End of scrolling to gallery script 
 			
    $('.howTo--js').click(function() {
       $('html, body').animate( {scrollTop:$('.section-steps--js').offset().top}, 1000);
-});   /* End of scrolling to how to/steps script */
+});   // End of scrolling to how to/steps script 
 			
    $('.cities--js').click(function() {
       $('html, body').animate( {scrollTop:$('.section-cities--js').offset().top}, 1000);
-});   /* End of scrolling to cities script */
+});   // End of scrolling to cities script 
 	
    $('.contact--js').click(function() {
       $('html, body').animate( {scrollTop:$('.section-contactform--js').offset().top}, 1000);
-});   /* End of scrolling to contactForm script */
+});   // End of scrolling to contactForm script 
 		
-/* Toggle */
+// Toggle 
    $('.hamburger--js').click(function() {
        var icon =  $('.hamburger--js i');
        $('.main-navigation--js').toggleClass('hideMe'); 
@@ -58,26 +58,26 @@ var waypoints = $('#handler-first').waypoint(function(direction) {
 	    icon.addClass('fa-bars');
 	}
     });
-/* ANIMATE */
-/* Feature columns fadeIn */
+// ANIMATE 
+// Feature columns fadeIn 
 	$('.feature-items--js').waypoint(function(direction) {
 	    $('.feature-items--js').addClass('animated fadeIn');
           }, {
 	      offset: '50%'
 	     });
-/* Cities columns fadeIn */	   
+// Cities columns fadeIn 	   
         $('.cities-box--js').waypoint(function(direction) {
 	    $('.cities-box--js').addClass('animated fadeIn');
         }, {
 	     offset: '50%'
 	   });
-/* iPhone in 'How It Works' section fadeInUp */	   
+// iPhone in 'How It Works' section fadeInUp   
 	$('.mobile--js').waypoint(function(direction) {
 	    $('.mobile--js').addClass('animated fadeInUp');
        }, {
            offset: '50%'
 	 });
-/* Plans columns pulse */	   
+// Plans columns pulse 	   
         $('.plans-item--js').waypoint(function(direction) {
 	    $('.plans-item--js').addClass('animated pulse');
 	}, {

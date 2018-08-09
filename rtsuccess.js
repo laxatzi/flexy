3e389b1a12119bcf5277b96f704fb0f8d0fa6b4c
Lambros Chatzinikolaou <39900010+laxatzi@users.noreply.github.com>
2018-08-09 11:24:56 +0300

    Update rtsuccess.js

--- rtsuccess.js
+++ rtsuccess.js
@@ -1,8 +1,8 @@
 function toggleFunc() {
-			      var menuItem = document.querySelectorAll(".navbar-menu");
-				  menuItem.forEach(function(arg) {
-						arg.classList.toggle("navbar-menu--show");
-				  })
-			 }
-			 document.querySelector(".navbar-toggle").addEventListener("click", toggleFunc);
-		</script>
+    var menuItem = document.querySelectorAll(".navbar-menu");
+    menuItem.forEach(function(arg) {
+        arg.classList.toggle("navbar-menu--show");
+    })
+}
+    document.querySelector(".navbar-toggle").addEventListener("click", toggleFunc);
+		
